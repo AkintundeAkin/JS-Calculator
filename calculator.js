4438c509d768a449efb7c7fe6ee7e88e40dd5d6c
AkintundeAkin <66368540+AkintundeAkin@users.noreply.github.com>
2022-06-25 12:29:27 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,31 @@
+
+function addition(){
+    a=Number(document.my_cal.first.value);
+    b=Number(document.my_cal.second.value);
+    c=a+b;
+    document.my_cal.total.value=c;
+  }
+
+   
+
+  function subtraction(){
+    a=Number(document.my_cal.first.value);
+    b=Number(document.my_cal.second.value);
+    c=a-b;
+    document.my_cal.total.value=c;
+  }
+
+  function multiply(){
+    a=Number(document.my_cal.first.value);
+    b=Number(document.my_cal.second.value);
+    c=a*b;
+    document.my_cal.total.value=c;
+  }
+
+
+  function division(){
+    a=Number(document.my_cal.first.value);
+    b=Number(document.my_cal.second.value);
+    c=a/b;
+    document.my_cal.total.value=c;
+  }
